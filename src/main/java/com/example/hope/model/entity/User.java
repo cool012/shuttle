5029684aeef1ee09f195bddbe95349c698c01b5b
TouwaErioer <duhongyu627@gmail.com>
2020-10-23 19:48:12 +0800

    创建用户实体类

---
+++ src/main/java/com/example/hope/model/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.hope.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @description: 用户实体类
+ * @author: DHY
+ * @created: 2020/10/23 19:43
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    private long id;
+    private String name;
+    private String encryption_password;
+    private String email;
+    private String address;
+    private boolean user_type;
+    private boolean is_admin;
+    private int score;
+
+}

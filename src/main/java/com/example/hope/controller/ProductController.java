d5f5868f0ee77d569d9534cfa987d6db9a9af1e4
TouwaErioer <duhongyu627@gmail.com>
2020-10-23 21:12:07 +0800

    修改

--- src/main/java/com/example/hope/controller/ProductController.java
+++ src/main/java/com/example/hope/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.example.hope.controller;
 
+import com.example.hope.common.utils.ReturnMessageUtil;
 import com.example.hope.model.entity.Product;
+import com.example.hope.model.entity.ReturnMessage;
 import com.example.hope.service.ProductService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -23,31 +25,34 @@ public class ProductController {
 
     @ApiOperation("添加产品")
     @RequestMapping(value = "/insert",method = RequestMethod.POST)
-    public void insert(Product product){
+    public ReturnMessage<Object> insert(Product product){
         productService.insert(product);
+        return ReturnMessageUtil.sucess();
     }
 
     @ApiOperation("删除产品")
     @RequestMapping(value = "/delete/{id}",method = RequestMethod.DELETE)
-    public void delete(@PathVariable("id") long id){
+    public ReturnMessage<Object> delete(@PathVariable("id") long id){
         productService.delete(id);
+        return ReturnMessageUtil.sucess();
     }
 
     @ApiOperation("修改产品")
     @RequestMapping(value = "/update",method = RequestMethod.POST)
-    public void updateProduct(Product product){
+    public ReturnMessage<Object> updateProduct(Product product){
         productService.update(product);
+        return ReturnMessageUtil.sucess();
     }
 
     @ApiOperation("按类型查找全部产品")
     @RequestMapping(value = "/findAllByType/{serviceId}",method = RequestMethod.GET)
-    public List<Product> findAllByType(@PathVariable("serviceId") long serviceId){
-        return productService.findAllByType(serviceId);
+    public ReturnMessage<Object> findAllByType(@PathVariable("serviceId") long serviceId){
+        return ReturnMessageUtil.sucess(productService.findAllByType(serviceId));
     }
 
     @ApiOperation("查找全部产品")
     @RequestMapping(value = "/findAll",method = RequestMethod.GET)
-    public List<Product> findAll(){
-        return productService.findAll();
+    public ReturnMessage<Object> findAll(){
+        return ReturnMessageUtil.sucess(productService.findAll());
     }
 }

b6594e7d6e8ac6d0a548d79c8e38d5008b670a5a
TouwaErioer <duhongyu627@gmail.com>
2020-10-23 21:13:28 +0800

    添加序列化接口

--- src/main/java/com/example/hope/model/entity/Product.java
+++ src/main/java/com/example/hope/model/entity/Product.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Product {
+public class Product implements Serializable {
 
     private long id;
     private String product_name;

6533b594eb1b53ddb53538419a174c804ca9f5e8
TouwaErioer <duhongyu627@gmail.com>
2021-02-20 18:16:45 +0800

    fix:ads实现Serializable

--- src/main/java/com/example/hope/model/entity/Ads.java
+++ src/main/java/com/example/hope/model/entity/Ads.java
@@ -2,13 +2,15 @@ package com.example.hope.model.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @description: 广告实体类
  * @author: DHY
  * @created: 2021/02/18 17:15
  */
 @Data
-public class Ads {
+public class Ads implements Serializable {
     private long id;
     private String image;
     private long storeId;
